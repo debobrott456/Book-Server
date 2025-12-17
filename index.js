c7cf3250b3687080b26e7cd24f321de7943e1c2d
Joy kumar <joyk3075@gmail.com>
2025-12-17 22:53:02 +0600

    added admin page api

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+  
     // await client.connect();
 const myDB4 = client.db("myDB4");
 const myColl4 = myDB4.collection("collBooks");
